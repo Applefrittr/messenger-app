bbcf1809601c77452f2a502fb94b6a5d0c1fe753
applefrittr <jacob.lee.jordan@gmail.com>
2023-10-04 15:51:54 -0400

    Remove friend functionality in FriendList added

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -3,12 +3,38 @@ import { useEffect, useState } from "react";
 function FriendList(props) {
   const [friends, setFriends] = useState();
 
+  const handleRemove = async (e) => {
+    const request = await fetch(
+      `http://localhost:5000/users/${props.user.username}/friends/${e.target.value}/remove`,
+      {
+        mode: "cors",
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${props.token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const response = await request.json();
+
+    console.log(response.message);
+
+    // Update the logged in User by calling updateUser from Dashboard.  This will ensure the fiends list will be updated in thie UI
+    const requestUser = await fetch(
+      `http://localhost:5000/users/${props.user.username}/profile`
+    );
+
+    const responseUser = await requestUser.json();
+
+    props.updateUser(responseUser.user);
+  };
+
   useEffect(() => {
     const friendsArray = [];
 
     props.user.friends.forEach((friend) => {
       friendsArray.push(
-        <div className="friend-card">
+        <div className="friend-card" key={friend.username}>
           <div className="friend-card-info">
             <div className="friend-avatar">
               <img src={friend.avatar} alt="avatar" />
@@ -19,7 +45,9 @@ function FriendList(props) {
           </div>
           <div className="friend-card-btns">
             <button value={friend.username}>Profile</button>
-            <button>Remove</button>
+            <button value={friend.username} onClick={handleRemove}>
+              Remove
+            </button>
           </div>
         </div>
       );
